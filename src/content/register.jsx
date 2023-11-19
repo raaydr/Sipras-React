36c08a3a1125b069a07e0c19f89382e0ddaa1fae
raaydr <dharmawanreynaldi@gmail.com>
2023-11-19 19:53:52 +0700

    hapus datepicker flowbite

--- src/content/register.jsx
+++ src/content/register.jsx
@@ -1,7 +1,7 @@
 import React, { useState,useEffect,useRef  } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 import useAuthContext from "../context/AuthContext";
-import Datepicker from 'flowbite-datepicker/Datepicker';
+
 const Register = () => {
     const [name, setName]= useState("");
     const [email, setEmail]= useState("");
@@ -14,12 +14,7 @@ const Register = () => {
     useEffect(() => {
         setRegis(null)
         setErrors([])
-        const datepickerEl = document.getElementById('datepickerId');
-new Datepicker(datepickerEl, {
-            
-            format: "dd/mm/yyyy",
-            autohide: true,
-        });
+        
       }, []);
 
       const data = {name,email,password,password_confirmation,recaptchaValue};
@@ -30,7 +25,7 @@ new Datepicker(datepickerEl, {
         captcha.current.reset()
     }
     return (
-        <div className="container">
+        <div className="container mx-auto">
        
         <form onSubmit={HandleRegister}>
              <div className="mb-6">
@@ -98,23 +93,7 @@ new Datepicker(datepickerEl, {
                  />
                  {errors.password_confirmation && <span style={{ color: 'red' }}>{errors.password_confirmation}</span>}
              </div>
-             <div className="mb-6">
-             <label
-                 htmlFor="password" 
-                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                 >
-                 Tanggal
-                 </label>
-                 <input
-                 
-                 
-                 type="text" onChange={(e)=>setPasswordConfirmation(e.target.value)} value={tanggal} name='tanggal'
-                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                 required="" id="datepickerId" datepicker
-                 datepicker-autohide 
-                 />
-                 {errors.password_confirmation && <span style={{ color: 'red' }}>{errors.password_confirmation}</span>}
-             </div>
+             
              <div className="mb-6">
              <ReCAPTCHA
                 ref={captcha}
