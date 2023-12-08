01379213030080b1984f436fcd614155f445896e
raaydr <dharmawanreynaldi@gmail.com>
2023-12-08 21:01:28 +0700

    hapus bagian gk terpakai di authcontext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -95,88 +95,8 @@ export const AuthProvider =({children}) => {
         }
     };
 
-    //indikator
-    const [fetchStatus, setFetchStatus] = useState(true)
-    //indikator
-    const [currentId, setCurrentId] = useState(-1)
     
-    const Rupiah = (angka) =>{
-        if(angka == null|| angka === 0){
-            return "Gratis";
-        } else{
-            let rupiah = angka.toLocaleString('id-ID', { style: 'currency', currency: 'IDR', minimumFractionDigits: 0, maximumFractionDigits: 0 });
-            return rupiah;
-            
-        }
-    }
-
-    const Platform = (params) => {
-        if(params != null){
-            if(params == 1) {
-              return  <p className="text-blue-600">HIRING</p>;
-              } else{
-                 
-                return  <p className="text-red-600">TUTUP</p>;
-              }
-          }
-    }
-
-    const createdata = async ({...data}) =>{
-        
-        try {
-            
-            const token = Cookies.get('tokenku')
-            const response = await axios.post("/api/create-barang", data,{ headers: {"Authorization" : `Bearer ${token}`} })
-            
-            
-            setCurrentId(-1);
-            setFetchStatus(true)
-            
-        } catch (e) {
-            if(e.response.status === 422){
-                setErrors(e.response.data.errors)
-
-            } else if (e.response.status === 404){
-                setErrors(e.response.data.data)
-            }
-        }
-    }
-    const editdata = async ({...data},currentId) =>{
-        
-        try {
-            const token = Cookies.get('tokenku')
-            const response = await axios.patch(`/api/update-barang/${currentId}`, data,{ headers: {"Authorization" : `Bearer ${token}`} })
-            
-            setCurrentId(-1);
-            setFetchStatus(true)
-            
-        } catch (e) {
-            if(e.response.status === 422){
-                setErrors(e.response.data.errors)
-
-            } else if (e.response.status === 404){
-                setErrors(e.response.data.data)
-            }
-        }
-    }
-    const deletedata = async (currentId) =>{
-        
-        try {
-            const token = Cookies.get('tokenku')
-            const response = await axios.delete(`/api/delete-barang/${currentId}`,{ headers: {"Authorization" : `Bearer ${token}`} })
-            setFetchStatus(true)
-            
-        } catch (e) {
-            if(e.response.status === 422){
-                setErrors(e.response.data.errors)
-
-            } else if (e.response.status === 404){
-                setErrors(e.response.data.data)
-            }
-        }
-    }
     return <AuthContext.Provider value={{user,errors,regis,setUser,setRegis,setErrors,getUser,login,logout,register,csrf,navigate,
-        fetchStatus, setFetchStatus,currentId, setCurrentId,Rupiah,Platform, createdata, editdata, deletedata
     }}>
         {children}
     </AuthContext.Provider>
